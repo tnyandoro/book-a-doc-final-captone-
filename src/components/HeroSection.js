0acd57c3ecbde15984738e111878d2f63d908d06
Tendai <tnyandoro@gmail.com>
2021-10-21 12:54:08 +0200

    add the sidebar

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,6 @@ import React from "react";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
